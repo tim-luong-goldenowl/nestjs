896b21a2438e551856a8a0fe08c1da0651d6378e
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-07-30 23:05:20 +0700

    7/30/2023 add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+    }
+  );
   await app.listen(3000);
 }
 bootstrap();
